d3000db49fd3e05bc3867f5c38ee9eb13cb75ea7
rohangupta <rohange@outlook.com>
2021-09-11 23:50:59 +0530

    2nd Dice Added

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -18,10 +18,14 @@ class MainActivity : AppCompatActivity() {
     private fun rollDice() {
         // Create new dice object with 6 slides and Roll it
         val dice = Dice(6)
+        val dice2 = Dice(6)
         val diceRoll = dice.roll()
+        val diceRoll2 = dice2.roll()
         // Update the screen with the text of diceRoll
         val resultTextView: TextView = findViewById(R.id.textView)
+        val resultTextView2: TextView = findViewById(R.id.textView2)
         resultTextView.text = diceRoll.toString()
+        resultTextView2.text = diceRoll2.toString()
     }
 }
 
