914eeee524037ed25756bb6036b78f2dd85595cc
rohangupta <rohange@outlook.com>
2021-09-13 10:41:02 +0530

    Conditional Behavior

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,7 +3,8 @@ package com.example.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
+import android.widget.ImageView
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,14 +19,10 @@ class MainActivity : AppCompatActivity() {
     private fun rollDice() {
         // Create new dice object with 6 slides and Roll it
         val dice = Dice(6)
-        val dice2 = Dice(6)
         val diceRoll = dice.roll()
-        val diceRoll2 = dice2.roll()
         // Update the screen with the text of diceRoll
-        val resultTextView: TextView = findViewById(R.id.textView)
-        val resultTextView2: TextView = findViewById(R.id.textView2)
-        resultTextView.text = diceRoll.toString()
-        resultTextView2.text = diceRoll2.toString()
+        val diceImage: ImageView = findViewById(R.id.imageView)
+        diceImage.setImageResource(R.drawable.dice_2)
     }
 }
 
